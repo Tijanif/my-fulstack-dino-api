d7e082d3e0512029583e7eb83dc6e5c11c518eab
Tijani Fulani <Fulani07@gmail.com>
2021-07-24 15:57:14 +0100

    fetch from server to client

--- client/src/components/dino.jsx
+++ client/src/components/dino.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './dino.css';
 
 const Dino = () => {
   console.log('I am running');
+  // const [dino, setDino] = useState;
+
+  // const fetchData = () => {
+  //   return fetch('https://randomuser.me/api/')
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // };
+
+  async function getDinoName() {
+    const res = await fetch('/dinoName');
+    const data = await res.json();
+    console.log(data);
+  }
+
+  // getDinoName();
+  useEffect(() => {
+    getDinoName();
+  }, []);
+
   return (
     <div>
       <h1>Welcome to Dino Generator</h1>
--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch');
 
 const app = express();
 
-app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    { id: 1, firstName: 'John', lastName: 'Doe' },
-    { id: 2, firstName: 'Brad', lastName: 'Traversy' },
-    { id: 3, firstName: 'Mary', lastName: 'Swanson' },
-  ];
-
-  res.json(customers);
+app.get('/dinoName', async (req, res) => {
+  // run code stuff
+  const fetchApi = await fetch(
+    'https://alexnormand-dino-ipsum.p.rapidapi.com/?paragraphs=1&words=2&format=json',
+    {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '419efc62d7msh8186214d37ac7a1p1323fbjsn17dc140ebc41',
+        'x-rapidapi-host': 'alexnormand-dino-ipsum.p.rapidapi.com',
+      },
+    }
+  );
+  const dinoNameRes = await fetchApi.json();
+  console.log(dinoNameRes);
+  res.json(dinoNameRes);
 });
+
+// app.get('/api/customers', cors(), (req, res) => {
+//   const customers = [
+//     { id: 1, firstName: 'John', lastName: 'Doe' },
+//     { id: 2, firstName: 'Brad', lastName: 'Traversy' },
+//     { id: 3, firstName: 'Mary', lastName: 'Swanson' },
+//   ];
+
+//   res.json(customers);
+// });
 
 const port = 5000;
 app.listen(port, () => `Server running on port ${port}`);

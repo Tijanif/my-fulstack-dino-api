250a08c44b6fee5ecb5d90780bf909e723aa8d55
Tijani Fulani <Fulani07@gmail.com>
2021-07-24 15:31:01 +0100

    created dino component and added styles.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import Customers from './components/customers';
+import Dino from './components/dino';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
-        </header>
-        <Customers />
+      <div className='App'>
+        <Dino />
       </div>
     );
   }
---
+++ client/src/components/dino.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const Dino = () => {
+  console.log('I am running');
+  return (
+    <div>
+      <h1>Welcome to Dino Generator</h1>
+    </div>
+  );
+};
+
+export default Dino;

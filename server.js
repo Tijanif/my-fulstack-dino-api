ccdd85b995c11b1a89f0d9ae0ea55271792ea52e
Tijani Fulani <Fulani07@gmail.com>
2021-07-24 17:49:11 +0100

    added background

--- server.js
+++ server.js
@@ -43,15 +43,5 @@ app.get('/dinoImage', async (req, res) => {
   res.json(dinoImageRes);
 });
 
-// app.get('/api/customers', cors(), (req, res) => {
-//   const customers = [
-//     { id: 1, firstName: 'John', lastName: 'Doe' },
-//     { id: 2, firstName: 'Brad', lastName: 'Traversy' },
-//     { id: 3, firstName: 'Mary', lastName: 'Swanson' },
-//   ];
-
-//   res.json(customers);
-// });
-
 const port = 5000;
 app.listen(port, () => `Server running on port ${port}`);
